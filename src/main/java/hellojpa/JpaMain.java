687e4afc331fa8d4e0d999055fba25f7ea06c9de
jeonhyunseok <sjunhs@gmail.com>
2022-03-26 20:20:59 +0900

    cannot find in first level cache, find it in database

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,19 +16,9 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            // 비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("Hello123");
-
-            // 영속
-            System.out.println("=== Before ===");
-            em.persist(member);
-            System.out.println("=== After ===");
-
-            Member findMember = em.find(Member.class, 101L);
-            System.out.println("findMember.id" + findMember.getId());
-            System.out.println("findMember.name" + findMember.getName());
+            // 똑같은 entity를 두번 조회할때, select 두번 실행되지 않는다.
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
 
             tx.commit();
         } catch (Exception e) {

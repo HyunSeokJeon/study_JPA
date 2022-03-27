1dcda55966ad7db3143eb56a2ff8e4f3efb81ade
jeonhyunseok <sjunhs@gmail.com>
2022-03-27 11:47:40 +0900

    first level cache not delete after flush

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,12 +16,19 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member = new Member(30L, "member30");
-            em.persist(member);
+            Member member1 = new Member(40L, "member40");
+            Member member2 = new Member(50L, "member50");
+            Member member3 = new Member(60L, "member60");
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
 
             em.flush();
-
             System.out.println("============================");
+
+            Member findMember1 = em.find(Member.class, 40L);
+            Member findMember2 = em.find(Member.class, 50L);
+            Member findMember3 = em.find(Member.class, 60L);
 
             tx.commit();
         } catch (Exception e) {

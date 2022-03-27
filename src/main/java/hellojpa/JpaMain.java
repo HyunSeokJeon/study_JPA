ce7e4f1e8615f0b369ca906368a57b89533e43a1
jeonhyunseok <sjunhs@gmail.com>
2022-03-27 14:36:10 +0900

    FlushModeType difference between commit and auto

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -13,15 +13,23 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member1 = new Member(70L, "member70");
-            Member member2 = new Member(80L, "member80");
-            Member member3 = new Member(90L, "member90");
+            Member member1 = new Member(110L, "member110");
+            Member member2 = new Member(120L, "member120");
+            Member member3 = new Member(1300L, "member130");
             em.persist(member1);
             em.persist(member2);
             em.persist(member3);
 
-            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-            List<Member> members = query.getResultList();
+            em.setFlushMode(FlushModeType.COMMIT);
+            List<Member> members1 = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
+            System.out.println("FlushModeType.COMMIT result size : " + members1.size());
+
+            em.setFlushMode(FlushModeType.AUTO);
+            List<Member> members2 = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
+
+            System.out.println("FlushModeType.AUTO result size : " + members2.size());
 
             tx.commit();
         } catch (Exception e) {

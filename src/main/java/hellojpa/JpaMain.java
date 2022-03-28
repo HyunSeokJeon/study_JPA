1ec68ab1e42f966b96a08604af0996fa575ceec0
jeonhyunseok <sjunhs@gmail.com>
2022-03-28 21:38:00 +0900

    JPA field mapping

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -13,13 +13,6 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            // 1차 캐시에 찾고 없네? - db에서 찾고 영속성 컨텍스트에 추가
-            Member member = em.find(Member.class, 140L);
-            member.setName("TEST");
-
-            // 영속 -> 준영속 jpa 가 더이상 관리하지 않음
-            em.detach(member);
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();

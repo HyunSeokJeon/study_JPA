03128320ec3468af0d27feb93aae5d19b3643280
jeonhyunseok <sjunhs@gmail.com>
2022-04-03 13:38:15 +0900

    table generator sequence

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,6 +4,12 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ",
+        allocationSize = 1
+)
 public class Member {
 
     /**
@@ -11,11 +17,11 @@ public class Member {
      * @GeneratedValue : 자동 생성
      * IDENTITY : 데이터베이스에 위임, MYSQL
      * SEQUENCE : 데이터베이스 시퀀스 오브젝트 사용, Oracle @SequenceGenerator
-     * TABLE : 키 생성용 테이블 사용, 모든 DB에서 사용
+     * TABLE : 키 생성용 테이블 사용, 모든 DB에서 사용가능 단점 : 최적화 이슈 성능 이슈
      * AUTO : 방언에 따라 자동 지정, 기본값
      */
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     @Column(name = "name")
